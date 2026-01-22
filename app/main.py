d18336fd333efa43841a0d628b99515d3e55d67c
chinmai-sd-123 <chinmaisdinesh@gmail.com>
2026-01-23 02:05:09 +0530

    Deploy Fastapi churn prediction

---
+++ app/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI,HTTPException
+import pandas as pd
+import numpy as np
+import joblib
+from tensorflow.keras.models import load_model
+from app.schemas import CustomerInput
+
+app = FastAPI(title="Bank Customer Churn Prediction ")
+
+model = load_model("models/churn_ann_model.h5", compile=False)
+scaler = joblib.load("models/scaler.pkl")
+
+FEATURE_ORDER= [
+    'CreditScore',
+    'Age',
+    'Tenure',
+    'Balance',
+    'NumOfProducts',
+    'HasCrCard',
+    'IsActiveMember',
+    'EstimatedSalary',
+    'Geography_Germany',
+    'Geography_Spain',
+    'Gender_Male'
+]
+
+threshold=0.50
+
+@app.get("/")
+
+def health_check():
+    return {"status": "api is running"}
+
+@app.post("/predict")
+
+def predict_churn(data: CustomerInput):
+
+    X_df=pd.DataFrame([data.model_dump()], columns=FEATURE_ORDER)
+    X_df_scaled=scaler.transform(X_df)
+    prob= model.predict(X_df_scaled)[0][0]
+
+    if np.isnan(prob) or np.isinf(prob):
+        raise HTTPException(status_code=400, detail="Model produced an invalid prediction. Check input values")
+
+    return {"churn_probability": float(prob),
+            "churn": int(prob >= threshold),
+            }
+
+
---
+++ app/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class CustomerInput(BaseModel):
+    CreditScore: int
+    Age: int
+    Tenure: int
+    Balance: float
+    NumOfProducts: int
+    HasCrCard: int
+    IsActiveMember: int
+    EstimatedSalary: float
+    Geography_Germany: int
+    Geography_Spain: int
+    Gender_Male : int
